262c5c2438f4d876f220413dabd606659114b870
bale1017 <bale1017@h-ka.de>
2023-06-29 16:37:14 +0200

    Merge Fixes

--- Assets/Health.cs
+++ Assets/Health.cs
@@ -71,7 +71,7 @@ public class Health : MonoBehaviour
         } else
         {
             Action<GameObject> increase = g => {
-                health += val;
+                g.GetComponent<Health>().health += val;
                 OnHealthChange.Invoke(health);
                 if (val < 0)
                 {
